095ae8d6f8373fc90d255830bda7fa59dfa49e0f
mountaindboz <David.Bosworth@water.ca.gov>
2020-05-21 10:06:40 -0700

    Added some code to export formatted data as a .csv file; added some useful comments

--- Water_Quality/Clean_RTM_Hydstra_example.R
+++ Water_Quality/Clean_RTM_Hydstra_example.R
@@ -14,22 +14,33 @@ library(lubridate)
 sharepoint_path <- normalizePath(
   file.path(
     Sys.getenv("USERPROFILE"),
-    "California Department of Water Resources/Office of Water Quality and Estuarine Ecology - North Delta Flow Action/WQ_Subteam/Raw_Data/Continuous"
+    "California Department of Water Resources/Office of Water Quality and Estuarine Ecology - North Delta Flow Action/WQ_Subteam"
   )
 )
 
 # Import data
 i80_orig <- read_csv(
-  file = paste0(sharepoint_path, "/RTM_RAW_DWR I80_2013-2019.csv"),
+  file = paste0(sharepoint_path, "/Raw_Data/Continuous/RTM_RAW_DWR I80_2013-2019.csv"),
   col_names = FALSE,
   skip = 3,
-  col_types = "cdd-------dd-dd-dd-dd-dd-"
+  col_types = "cdd-------dd-dd-dd-dd-dd-"  # "c" = character, "d" = numeric, "-" = skip
 ) 
 
 glimpse(i80_orig)
 
+# HYDSTRA PARAMETER CODES:
+  # 450 - Water Temperature (Celcius)
+  # 630 - Depth below water surface (meters)
+  # 806 - Salinity (ppt)
+  # 810 - Turbidity (NTU)
+  # 821 - Specific Conductance at 25 C (uS/cm)
+  # 860 - pH
+  # 865 - Dissolved Oxygen (% saturation)
+  # 2351 - Dissolved Oxygen (mg/L)
+  # 7004 - Chlorophyll (ug/L)
+
 # Clean data
-  # Change variable names
+  # Change variable names - using NDFA standardized names
   names(i80_orig) <- c(
     "DateTime",
     "WaterTemp",
@@ -54,4 +65,15 @@ glimpse(i80_orig)
     )
 
   glimpse(i80_clean)
+
+# Export formatted data as a .csv file 
+i80_clean %>% 
+  write_excel_csv(
+    path = paste0(sharepoint_path, "/Processed_Data/Continuous/RTM_OUTPUT_I80_formatted.csv"),
+    na = ""
+  )
+
+# For easier importing of this file in the future should either:
+# 1) convert file to .xlsx file after exporting, or
+# 2) manually format the 'DateTime' variable in the .csv file to "yyyy-mm-dd hh:mm:ss"
 
