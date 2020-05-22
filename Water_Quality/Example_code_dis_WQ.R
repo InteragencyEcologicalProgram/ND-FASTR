27522687fc8a617713b12a7c6305273ad71cf98a
jrinde <44582450+jrinde@users.noreply.github.com>
2020-05-22 09:15:32 -0700

    Add files via upload

---
+++ Water_Quality/Example_code_dis_WQ.R
@@ -0,0 +1,7 @@
+# Example code for working with discrete water quality data
+# Author: Jenna Rinde
+
+# Load packages- install these packages if you haven't already using install.packages()
+library(tidyverse)
+library(readxl)
+library(lubridate)
