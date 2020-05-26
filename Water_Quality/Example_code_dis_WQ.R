742871268095c3171c85c10a44dfc419c9bce657
Jenna Rinde <jenna.rinde@water.ca.gov>
2020-05-26 13:12:51 -0700

    Adding R script through R studio with Bos on the line

--- Water_Quality/Example_code_dis_WQ.R
+++ Water_Quality/Example_code_dis_WQ.R
@@ -5,5 +5,34 @@
 library(tidyverse)
 library(readxl)
 library(lubridate)
+library(dplyr)
 
-#adding more for a test
+# Import Data -------------------------------------------------------------
+
+# Define path on SharePoint site for data - this works if you have the SharePoint site synced
+# to your computer
+sharepoint_path <- normalizePath(
+  file.path(
+    Sys.getenv("USERPROFILE"),
+    "California Department of Water Resources/Office of Water Quality and Estuarine Ecology - North Delta Flow Action/WQ_Subteam/Raw_Data/Discrete"
+  )
+)
+
+# 1. Importing nutrient data from SharePoint
+
+
+nuts <- read_excel(
+  path = paste0(sharepoint_path, "/DWQ_nuts_raw.xlsx"), sheet = "WQData (11)"
+)
+
+head(nuts)
+
+str(nuts)
+
+#example of old code for different study for subsetting by station
+
+CM40<- subset(CM, station== "CM40")
+CM42<- subset(CM, station== "CM42")
+CM43<- subset(CM, station== "CM43")
+CM48<- subset(CM, station== "CM48")
+RRI<- subset(CM, station== "CM42_RRI")
