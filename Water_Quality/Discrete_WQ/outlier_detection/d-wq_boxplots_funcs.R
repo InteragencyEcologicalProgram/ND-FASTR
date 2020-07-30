62fb5a0cf5b96fd973814ec7ce4cb671a74035a2
seperry83 <seperry83@gmail.com>
2020-07-30 03:18:18 -0700

    created base boxplots (math is right)

--- Water_Quality/Discrete_WQ/outlier_detection/d-wq_boxplots_funcs.R
+++ Water_Quality/Discrete_WQ/outlier_detection/d-wq_boxplots_funcs.R
@@ -76,43 +76,83 @@ add_phase_actions <- function(df_wq, df_dates){
 
 # --- Plot Boxplot ---
 # adapted from NADA package
-cen_boxplt <- function (obs, cen, group, log = TRUE, range = 0, ...) {
-  # determine if log scaled is used
-  if (log) {
-    log = "y" 
-  } else {
-    log = ""
-  }
+
+cen_boxplt <- function (df) {
   
-  # determine if obs are grouped
-  if (missing(group)) {
-    ret = boxplot(cenros(obs, cen), log = log, range = range,
-                  ...)
-  } else {
-    modeled = numeric()
-    groups = character()
+  # filter df by analyte
+  df_filt <-
+    df_wq %>%
+    filter(
+      Analyte == analyte,
+      StationCode == station
+    )
+  
+  # define vectors for cenfit function
+  obs <- df$Result
+  cen <- df$LabDetect
+  group <- df$FullGroup
+  
+  
+  data = numeric()
+  groups = character()
+  group_factor <- as.factor(group)
+  
+if(length(levels(group_factor)) > 0) { # if there's any data for any group
+
+  
+  # for each level of the group
+  for (i in levels(group_factor)) { 
     
-    for (i in levels(as.factor(group))) {
-      # if censored data, perform ROS model
-      if (length(cen[group == i]) > 1) {
-        mod = suppressWarnings(cenros(obs[group == i], cen[group ==
-                                                           i])$modeled)
-        grp = rep(i, length(mod))
-        modeled = c(modeled, mod)
+    # define the boolean where T = censored and F = uncensored for the group
+    uncen_data <- cen[group == i]
+    
+    if (length(uncen_data) > 0) { # if there's any data
+      
+      if (all(uncen_data)) { # if it's all censored data
+        # skip to next group
+        next
+      }
+      else if (all(!uncen_data)) { # if all uncensored data
+        # populate the vectors using all values
+        data = c(data, obs[group == i])
+        grp = rep(i, length(obs[group == i]))
         groups = c(groups, grp)
       }
+      else { # if mix between censored/uncensored data
+        # populate the vectors using ROS model values
+        mod = suppressWarnings(cenros(obs[group == i], cen[group == i])$modeled)
+        grp = rep(i, length(mod))
+        data = c(data, mod)
+        groups = c(groups, grp)
+      }
+
       }
   }
-  # plot boxplots
-  if (length(cen[group == i]) > 1) {
-    boxplot(modeled ~ as.factor(groups), log = log, range = range,
-            ...)
-    ret = data.frame(ros.model = modeled, group = groups)
-  } else{
-    boxplot(obs ~ as.factor(groups), log = log, range = range,
-             ...)
-    ret = data.frame(data = obs, group = groups)
+      
+      # plot boxplots
+      # if (length(cen[group == i]) > 1) {   # if there's censored data
+        # boxplot(data ~ as.factor(groups), log = log, range = range,
+        #         ...)
+        ret = data.frame(data = data, group = groups)
+        # print(ret$group)
+        bp <- ggplot(ret, aes(x=group, y=data, group=group)) +
+          geom_boxplot(aes(fill=group))
+        print(bp)
+        # invisible(ret)
+        # return(bp)
+      # }
+      # } else{ #if there isn't
+      #   # print(i)
+      #   ret = data.frame(ros.model = obs, group = groups)
+      #   bp <- ggplot(ret, aes(x=group, y=ros.model, group=group)) +
+      #     geom_boxplot(aes(fill=group))
+      #   print(bp)
+      #   # boxplot(obs ~ as.factor(groups), log = log, range = range,
+      #   #         ...)
+      #   #ret = data.frame(data = obs, group = groups)
+      #   invisible(ret)
+      #   return(bp)
+      # }
+    }
   }
-  abline(h = max(obs[cen]))
-  invisible(ret)
 }
