ac586393137a5e628ceedc48c045a9c70c526dc9
mountaindboz <David.Bosworth@water.ca.gov>
2020-08-10 17:24:02 -0700

    Updated script to download continuous water quality data from USGS- prior method didn't work properly

--- Water_Quality/Continuous_WQ/USGS_RTM_Data_Download.R
+++ Water_Quality/Continuous_WQ/USGS_RTM_Data_Download.R
@@ -6,6 +6,9 @@
 library(tidyverse)
 library(dataRetrieval)
 
+# Source global WQ functions
+source("Water_Quality/global_wq_funcs.R")
+
 # Create vectors for parameters, start and end dates
 start_date <- "2011-01-01" 
 end_date <- "2019-12-31" 
@@ -24,63 +27,49 @@ params <- c(
   "99133"   # Nitrate plus nitrite (mg/L as N)
 )
 
-# Create a dataframe for downloading data for each site
-usgs_data <- tibble(
-  site_name = c(
-    "RD22",
-    "RVB",
-    "LIB",
-    "RYI_1",
-    "RYI_2",
-    "TOE_1",
-    "TOE_2",
-    "SDI",
-    "SGG",
-    "LIBCUT"
-  ),
-  site_numb = c(
-    "11453000",
-    "11455420",
-    "11455315",
-    "11455350",
-    "11455385",
-    "11455140",
-    "11455139",
-    "11455478",
-    "11455276",
-    "11455146"
-  )
-)
+# Download data for each station individually since doing it all at once doesn't work correctly
+rd22 <- readNWISuv("11453000", params, start_date, end_date, tz = "Etc/GMT+8")
+rvb <- readNWISuv("11455420", params, start_date, end_date, tz = "Etc/GMT+8")
+lib <- readNWISuv("11455315", params, start_date, end_date, tz = "Etc/GMT+8")
+ryi1 <- readNWISuv("11455350", params, start_date, end_date, tz = "Etc/GMT+8")
+ryi2 <- readNWISuv("11455385", params, start_date, end_date, tz = "Etc/GMT+8")
+toe1 <- readNWISuv("11455140", params, start_date, end_date, tz = "Etc/GMT+8")
+toe2 <- readNWISuv("11455139", params, start_date, end_date, tz = "Etc/GMT+8")
+sdi <- readNWISuv("11455478", params, start_date, end_date, tz = "Etc/GMT+8")
+sgg <- readNWISuv("11455276", params, start_date, end_date, tz = "Etc/GMT+8")
+libcut <- readNWISuv("11455146", params, start_date, end_date, tz = "Etc/GMT+8")
 
-# Download data
-usgs_data <- usgs_data %>% 
-  mutate(
-    raw_data = map(
-      site_numb, 
-      .f = readNWISuv, 
-      parameterCd = params, 
-      start_date, 
-      end_date, 
-      tz = "Etc/GMT+8"
-    ),
-    raw_data_t = map(raw_data, .f = as_tibble)
-  )
+# Combine all data files into a named list and batch convert them to tibbles 
+usgs_data <- 
+  list(
+    RD22 = rd22,
+    RVB = rvb,
+    LIB = lib,
+    RYI_1 = ryi1,
+    RYI_2 = ryi2,
+    TOE_1 = toe1,
+    TOE_2 = toe2,
+    SDI = sdi,
+    SGG = sgg,
+    LIBCUT = libcut
+  ) %>% 
+  map(as_tibble)
+  
+# Define main NDFA file path for WQ subteam (assumes synced with SharePoint)
+fp_fastr <- "California Department of Water Resources/Office of Water Quality and Estuarine Ecology - North Delta Flow Action/WQ_Subteam/"
 
-# Define path on SharePoint site to export data to
-sharepoint_path <- normalizePath(
-  file.path(
-    Sys.getenv("USERPROFILE"),
-    "California Department of Water Resources/Office of Water Quality and Estuarine Ecology - North Delta Flow Action/WQ_Subteam/Raw_Data/Continuous"
-  )
-)
+# Define relative file path to export raw continuous USGS data files to
+fp_rel_wq_raw <- paste0(fp_fastr, "Raw_Data/Continuous")
+
+# Define absolute file path
+fp_abs_wq_raw <- get_abs_path(fp_rel_wq_raw)
 
 # Export raw data as .csv files for each site
-walk2(
-  usgs_data$raw_data_t,
-  usgs_data$site_name,
+iwalk(
+  usgs_data,
   .f = ~write_excel_csv(
     .x, 
-    path = paste0(sharepoint_path, "/RTM_RAW_USGS_", .y, ".csv"),
+    path = paste0(fp_abs_wq_raw, "/RTM_RAW_USGS_", .y, ".csv"),
     na = ""
   )
 )
