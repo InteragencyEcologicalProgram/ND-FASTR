1aaeb78b00ed508dd0db01bda48861d1c417c87c
mountaindboz <53193874+mountaindboz@users.noreply.github.com>
2020-09-25 12:37:06 -0700

    Finished script for cleaning all contaminants water concentration data for 2015-2020

--- Water_Quality/Contaminants/contam_clean_data_funcs.R
+++ Water_Quality/Contaminants/contam_clean_data_funcs.R
@@ -11,16 +11,29 @@ library(readxl)
 # Data Import Functions ---------------------------------------------------
 
 # Import USGS contaminants data from SharePoint that was downloaded from NWIS
-import_nwis_data <- function(file_path){
+import_nwis_data <- function(file_path) {
   df <- read_csv(
     file = file_path,
-    col_types = "---------c--------ncd-ccdc-----c-c-"
+    col_types = "c--c------c--------ncn---n------c---"
   )
   
   return(df)
 }
 
 
-# Standardizing functions -------------------------------------------------
+# Cleaning functions -------------------------------------------------
 
+# Convert NA values, where NA (missing) values represent measurements below the Method Detection 
+  # Limit and "NA" values represent instances when the parameter wasn't analyzed
+convert_na_val <- function(df, var_idx_start, var_idx_end) {
+  df %>% 
+    mutate_at(
+      c(var_idx_start:var_idx_end),
+      ~case_when(
+        is.na(.x) ~ "Non-detect",
+        .x == "NA" ~ "Not analyzed",
+        TRUE ~ .x
+      )
+    )
+}
 
