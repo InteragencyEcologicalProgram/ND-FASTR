43ac1bd4b1685246611e825b76acd74e5e429d79
mountaindboz <53193874+mountaindboz@users.noreply.github.com>
2020-11-07 00:49:45 +0000

    Adding some additional functions to streamline the plotting of the continuous raw data

--- Water_Quality/Continuous_WQ/Plots_Raw_Data/rtm_raw_plots_funcs.R
+++ Water_Quality/Continuous_WQ/Plots_Raw_Data/rtm_raw_plots_funcs.R
@@ -6,9 +6,11 @@
 # Load packages
 library(tidyverse)
 library(scales)
+library(plotly)
+
 
 # Import processed continuous WQ Data from SharePoint
-import_rtm_data <- function(file_path, num_params){
+import_rtm_data <- function(file_path, num_params) {
   df <- read_csv(
     file = file_path,
     col_types = paste0("cc", str_dup("dc", num_params))
@@ -18,35 +20,75 @@ import_rtm_data <- function(file_path, num_params){
 }
 
 
-# Add variable for y-axis labels in plots
-add_yaxis_var <- function(df, param_var) {
-  # Convert param_var to quosure for tidy evaluation
-  param_var_enquo <- enquo(param_var)
-  
-  # Add variable to dataframe
-  df1 <- df %>% 
-    mutate(
-      yaxis_lab = case_when(
-        !!param_var_enquo == "Chla" ~ "Chlorophyll (ug/L)",
-        !!param_var_enquo == "Chla_RFU" ~ "Chlorophyll Fluorescence (RFU)",
-        !!param_var_enquo == "DO" ~ "Dissolved Oxygen (mg/L)",
-        !!param_var_enquo == "fDOM" ~ "Dissolved Organic Matter Fluorescence (ug/L as QSE)",
-        !!param_var_enquo == "Flow" ~ "Flow (cfs)",
-        !!param_var_enquo == "FlowTF" ~ "Tidally-filtered Flow (cfs)",
-        !!param_var_enquo == "NitrateNitrite" ~ "Nitrate + Nitrite (mg/L as N)",
-        !!param_var_enquo == "pH" ~ "pH (pH units)",
-        !!param_var_enquo == "SpCnd" ~ "Specific Conductance (uS/cm)",
-        !!param_var_enquo == "Turbidity" ~ "Turbidity (FNU)",
-        !!param_var_enquo == "WaterTemp" ~ "Water Temperature (degrees C)",
-      )
+# Restructure data to long format to allow for a more generalized plotting function
+restructure_df_long <- function(df) {
+  df %>% 
+    select(-c(StationCode, ends_with("_Qual"))) %>% 
+    pivot_longer(
+      cols = -DateTime,
+      names_to = "parameter",
+      values_to = "value"
     )
-  
-  return(df1)
 }
 
 
-# Create simple timeseries plots of continuous WQ Data
-create_ts_plot <- function(df, y_lab) {
+# Remove data before the first year when data was collected for each parameter so 
+  # that the x-axis is centered correctly on the plots of all years of data
+trim_to_first_yr <- function(df) {
+  # Find the first year with data for each parameter
+  data_first_yr <- df %>% 
+    filter(!is.na(value)) %>% 
+    group_by(parameter) %>% 
+    summarize(min_yr = min(yr))
+  
+  
+  # Filter data in df
+  df %>% 
+    left_join(data_first_yr, by = "parameter") %>% 
+    filter(yr >= min_yr) %>% 
+    select(-min_yr)
+}
+
+
+# Add extra datetime values so that the lines connecting years are eliminated in the 
+  # plots of all years of data
+add_extra_dt_values <- function(df) {
+  extra_dt <- df %>% 
+    group_by(yr, parameter) %>% 
+    summarize(max_dt = max(DateTime), .groups = "drop") %>% 
+    mutate(DateTime = max_dt + minutes(15)) %>%  
+    select(-max_dt)
+  
+  bind_rows(df, extra_dt)  
+}
+
+
+# Internal function to define y-axis labels in plots
+int_define_yaxis_lab <- function(param) {
+  yaxis_lab <- case_when(
+    param == "Chla" ~ "Chlorophyll (ug/L)",
+    param == "Chla_RFU" ~ "Chlorophyll Fluorescence (RFU)",
+    param == "DO" ~ "Dissolved Oxygen (mg/L)",
+    param == "fDOM" ~ "Dissolved Organic Matter Fluorescence (ug/L as QSE)",
+    param == "Flow" ~ "Flow (cfs)",
+    param == "FlowTF" ~ "Tidally-filtered Flow (cfs)",
+    param == "NitrateNitrite" ~ "Nitrate + Nitrite (mg/L as N)",
+    param == "pH" ~ "pH (pH units)",
+    param == "SpCnd" ~ "Specific Conductance (uS/cm)",
+    param == "Turbidity" ~ "Turbidity (FNU)",
+    param == "WaterTemp" ~ "Water Temperature (degrees C)",
+  )
+  
+  return(yaxis_lab)
+}
+
+
+# Create simple ggplot of continuous WQ Data - for plots of all years of data
+create_ts_ggplot <- function(df) {
+  # define y-axis label
+  y_lab <- int_define_yaxis_lab(df$parameter[1])
+  
+  # create plot
   p <-
     ggplot(
       data = df,
@@ -67,5 +109,16 @@ create_ts_plot <- function(df, y_lab) {
     )
   
   return(p)
+}
+
+
+# Create simple interactive timeseries plotly plot of continuous WQ Data - for plots 
+  # of individual years
+create_ts_plotly <- function(df) {
+  # use create_ts_ggplot to make timeseries ggplot
+  p <- create_ts_ggplot(df)
+  
+  # convert ggplot to interactive plotly plot
+  ggplotly(p)
 }
 
