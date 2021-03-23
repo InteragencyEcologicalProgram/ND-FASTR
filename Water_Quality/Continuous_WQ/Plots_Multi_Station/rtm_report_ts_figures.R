d40de845e82d8b6a215f367e3627f3bb438fb4af
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-03-23 15:31:15 -0700

    Adding file to create report figures of time-series plots for the continuous WQ data

---
+++ Water_Quality/Continuous_WQ/Plots_Multi_Station/rtm_report_ts_figures.R
@@ -0,0 +1,244 @@
+# NDFA Water Quality
+# Purpose: Create time series plots of the continuous water quality data for the report figures
+# Author: Dave Bosworth & Amanda Maguire
+# Contact: David.Bosworth@water.ca.gov
+
+
+# Global Code and Functions -----------------------------------------------
+
+# Load packages
+library(tidyverse)
+library(lubridate)
+library(scales)
+library(patchwork)
+
+# Source functions
+source("global_ndfa_funcs.R")
+source("Water_Quality/global_wq_funcs.R")
+
+
+# Import and Prepare Data -------------------------------------------------
+
+# Define relative file path for file containing all QA'ed and cleaned continuous WQ data
+fp_rel_rtm_data <- "WQ_Subteam/Processed_Data/Continuous/RTM_INPUT_all_2021-02-19.csv"
+
+# Define relative file path for file containing dates of flow action periods
+fp_rel_fa_dates <- "Data Management/FlowDatesDesignations_45days.csv"
+
+# Define absolute file paths
+fp_abs_rtm_data <- ndfa_abs_sp_path(fp_rel_rtm_data)
+fp_abs_fa_dates <- ndfa_abs_sp_path(fp_rel_fa_dates)
+
+# Import continuous WQ data
+df_rtm_orig <- import_rtm_data(fp_abs_rtm_data, 10)
+
+# Import dates of flow action periods
+df_fa_dates_orig <- read_csv(fp_abs_fa_dates)
+
+# Create a vector for the factor order of StationCode
+sta_order <- c(
+  "RMB",
+  "RCS",
+  "RD22",
+  "I80",
+  "LIS",
+  "TOE",
+  "STTD",
+  "LIBCUT",
+  "SGG",
+  "LIB",
+  "RYI",
+  "RVB",
+  "SRH"
+)
+
+# Clean original continuous WQ data
+df_rtm_clean <- df_rtm_orig %>% 
+  mutate(
+    DateTime = ymd_hms(DateTime),
+    Date = date(DateTime),
+    Year = year(DateTime)
+  ) %>%
+  # Don't include Flow, FlowTF, and all Qual variables
+  select(-c(ends_with("_Qual"), starts_with("Flow"))) %>% 
+  # Exclude SDI from the plots and remove data for years 2011-2012
+  filter(
+    StationCode != "SDI",
+    Year > 2012
+  ) %>% 
+  # Apply factor order to StationCode
+  mutate(StationCode = factor(StationCode, levels = sta_order)) %>% 
+  # Pivot parameters in the long format
+  pivot_longer(cols = where(is.numeric) & !Year, names_to = "Parameter", values_to = "Value") %>% 
+  # Remove all NA values
+  filter(!is.na(Value))
+
+# Calculate daily averages of continuous WQ data
+df_rtm_daily_avg <- df_rtm_clean %>% 
+  group_by(Parameter, Year, StationCode, Date) %>% 
+  summarize(Daily_avg = mean(Value)) %>% 
+  ungroup()
+
+# Structure continuous WQ data to be able to apply plotting functions
+df_rtm_daily_avg_f <- df_rtm_daily_avg %>%
+  # Fill in missing dates with NA values for geom_line to not interpolate data gaps
+  group_by(Parameter, Year, StationCode) %>% 
+  complete(Date = seq.Date(min(Date), max(Date), by = "1 day")) %>% 
+  ungroup() %>% 
+  # Add a grouping variable for Region and apply factor order for it
+  mutate(
+    Region = factor(
+      case_when(
+        StationCode %in% c("RMB", "RCS", "RD22", "I80") ~ "a_Upstream_upper",
+        StationCode %in% c("LIS", "TOE", "STTD", "LIBCUT", "SGG") ~ "b_Upstream_central",
+        StationCode %in% c("LIB", "RYI", "RVB") ~ "c_Downstream",
+        StationCode == "SRH" ~ "d_Sac_River"
+      )
+    )
+  )
+
+# Prepare dates of flow action periods to highlight the flow action periods for each year 
+  # in the plots and to define consistent x-axis limits
+df_fa_dates_f <- df_fa_dates_orig %>% 
+  select(Year, starts_with(c("Pre", "Post"))) %>% 
+  mutate(across(where(is.character), mdy)) %>% 
+  filter(Year > 2012) %>% 
+  # add 1 day to PreFlowEnd so that the highlight for the flow action periods aligns correctly
+  mutate(PreFlowEnd = PreFlowEnd + days(1))
+
+
+# Load Plot Functions -----------------------------------------------------
+
+# Internal function to define y-axis labels in plots
+int_define_yaxis_lab <- function(param) {
+  yaxis_lab <- case_when(
+    param == "Chla" ~ "Chlorophyll (ug/L)",
+    param == "DO" ~ "Diss Oxygen (mg/L)",
+    param == "pH" ~ "pH (pH units)",
+    param == "SpCnd" ~ "Sp Cond (uS/cm)",
+    param == "Turbidity" ~ "Turbidity (FNU)",
+    param == "WaterTemp" ~ "Water Temp (C)",
+  )
+  
+  return(yaxis_lab)
+}
+
+# Internal function to define the min and max dates of the flow action periods 
+# and the x-axis limits for each year
+int_define_fa_dates <- function(yr) {
+  df_yr <- df_fa_dates_f %>% filter(Year == yr)
+  
+  list(
+    x_lim_min = df_yr$PreFlowStart,
+    x_lim_max = df_yr$PostFlowEnd,
+    action_min = df_yr$PreFlowEnd, 
+    action_max = df_yr$PostFlowStart
+  )
+}
+
+# Internal function to titles for each plot
+int_define_plot_title <- function(region) {
+  plot_title <- case_when(
+    region == "a_Upstream_upper" ~ "Upstream Region - CBD and Upper Yolo Bypass stations",
+    region == "b_Upstream_central" ~ "Upstream Region - Central and Lower Yolo Bypass stations",
+    region == "c_Downstream" ~ "Downstream Region",
+    region == "d_Sac_River" ~ "Middle Sacramento River"
+  )
+  
+  return(plot_title)
+}
+
+# Create simple ggplot of continuous WQ Data - for plots of all years of data
+create_ts_plot <- function(df, plot_pos = c("other", "bottom")) {
+  match.arg(plot_pos, c("other", "bottom"))
+  
+  # define y-axis label
+  y_lab <- int_define_yaxis_lab(df$Parameter[1])
+  
+  # define dates of flow action period and x-axis limits
+  fa_dates <- int_define_fa_dates(df$Year[1])
+  
+  # define plot label
+  plot_title <- int_define_plot_title(df$Region[1])
+  
+  # create base plot
+  p <- df %>% 
+    ggplot(aes(x = Date, y = Daily_avg, color = StationCode)) +
+    geom_line() +
+    theme_light() +
+    theme(panel.grid.minor = element_blank()) +
+    ggtitle(plot_title) +
+    scale_y_continuous(
+      name = y_lab,
+      labels = label_comma()
+    ) +
+    scale_color_viridis_d(
+      name = "Station:", 
+      option = "plasma", 
+      end = 0.9
+    ) +
+    annotate(
+      "rect", 
+      xmin = fa_dates$action_min, 
+      xmax = fa_dates$action_max, 
+      ymin = -Inf, 
+      ymax = Inf, 
+      alpha = 0.1,
+      fill = "brown"
+    )
+  
+  # apply formatting for x-axis based on plot_pos
+  if (plot_pos == "bottom") {
+    # only the bottom most plot gets x-axis labels and tick marks
+    p <- p +
+      scale_x_date(
+        name = "Date",
+        limits = c(fa_dates$x_lim_min, fa_dates$x_lim_max),
+        breaks = breaks_pretty(15),
+        labels = label_date_short(),
+        expand = expansion(mult = 0.01)
+      )
+  } else {
+    p <- p +
+      scale_x_date(
+        name = NULL,
+        limits = c(fa_dates$x_lim_min, fa_dates$x_lim_max),
+        breaks = breaks_pretty(15),
+        labels = NULL,
+        expand = expansion(mult = 0.01)
+      ) +
+      theme(axis.ticks.x = element_blank())
+  }
+  
+  return(p)
+}
+
+
+# Create Plots ------------------------------------------------------------
+
+# Chlorophyll 2013
+chla13 <- df_rtm_daily_avg_f %>% 
+  filter(
+    Parameter == "Chla",
+    Year == 2013
+  )
+
+chla13_top <- chla13 %>% 
+  filter(str_detect(Region, "^a_")) %>% 
+  create_ts_plot("other")
+
+chla13_mid1 <- chla13 %>% 
+  filter(str_detect(Region, "^b_")) %>% 
+  create_ts_plot("other")
+
+chla13_mid2 <- chla13 %>% 
+  filter(str_detect(Region, "^c_")) %>% 
+  create_ts_plot("other")
+
+chla13_bottom <- chla13 %>% 
+  filter(str_detect(Region, "^d_")) %>% 
+  create_ts_plot("bottom")
+
+chla13_top / chla13_mid1 / chla13_mid2 / chla13_bottom
+ggsave("Chla_2013.jpg", width = 7, height = 9.5, units = "in", dpi = 300)
+
