86c8076b4220461f8bb88bc01937b933da227e0c
Dave Bosworth <David.Bosworth@water.ca.gov>
2024-02-28 15:48:51 -0800

    Adding specific conductance to the processed daily and weekly average datasets

--- manuscript_synthesis/src/process_data_rtm_chla_wt.R
+++ manuscript_synthesis/src/process_data_rtm_chla_wt.R
@@ -1,8 +1,8 @@
 # NDFS Synthesis Manuscript
-# Purpose: Process the 15-minute continuous chlorophyll and water temperature
-  # data collected from the Yolo Bypass and downstream during years 2013-2019.
-  # Calculate daily and weekly averages which are used in figures and analysis for
-  # the NDFS synthesis manuscript.
+# Purpose: Process the 15-minute continuous chlorophyll, water temperature, and
+  # specific conductance data collected from the Yolo Bypass and downstream during
+  # years 2013-2019. Calculate daily and weekly averages which are used in figures
+  # and analysis for the NDFS synthesis manuscript.
 # Author: Dave Bosworth
 # Contacts: David.Bosworth@water.ca.gov
 
@@ -21,20 +21,23 @@ conflicts_prefer(dplyr::filter())
 # Check if we are in the correct working directory
 i_am("manuscript_synthesis/src/process_data_rtm_chla_wt.R")
 
+# Create vector of parameter names to be manipulated in later functions
+wq_params <- c("WaterTemp", "Chla", "SpCnd")
+
 
 # Import Continuous Data --------------------------------------------------
 
 # Define directory for the continuous WQ data on the NDFA SharePoint
 fp_rtm_wq <- ndfa_abs_sp_path("2011-2019 Synthesis Study-FASTR/WQ_Subteam/Processed_Data/Continuous")
 
-# Import QA'ed and cleaned continuous chlorophyll and water temperature data for
-  # the NDFS period of interest
+# Import QA'ed and cleaned continuous WQ data for the NDFS period of interest
 df_rtm_wq <- read_csv(
   file = path(fp_rtm_wq, "RTM_INPUT_all_2021-04-20.csv"),
   col_types = cols_only(
     StationCode = "c",
     DateTime = "c",
     WaterTemp = "d",
+    SpCnd = "d",
     Chla = "d"
   )
 )
@@ -60,31 +63,39 @@ df_rtm_wq_c <- df_rtm_wq %>%
 # Calculate Daily Averages ------------------------------------------------
 
 df_wq_daily_avg <- df_rtm_wq_c %>% 
-  # calculate daily average chlorophyll and water temperature values
-  summarize(across(c(WaterTemp, Chla), ~ mean(.x, na.rm = TRUE)), .by = c(StationCode, Date)) %>% 
+  # calculate daily average chlorophyll, water temperature, and specific
+    # conductance values
+  summarize(
+    across(all_of(wq_params), ~ mean(.x, na.rm = TRUE)), 
+    .by = c(StationCode, Date)
+  ) %>% 
   # convert NaN values to NA values
-  mutate(across(c(WaterTemp, Chla), ~ if_else(is.nan(.x), NA_real_, .x))) %>% 
-  # remove records where chlorophyll values are NA
-  drop_na(Chla) %>% 
+  mutate(across(all_of(wq_params), ~ if_else(is.nan(.x), NA_real_, .x))) %>% 
+  # remove rows where all WQ parameters are NA
+  filter(!if_all(all_of(wq_params), is.na)) %>% 
   arrange(StationCode, Date)
 
 
 # Calculate Weekly Averages -----------------------------------------------
 
 df_wq_week_avg <- df_rtm_wq_c %>% 
-  # calculate weekly average chlorophyll and water temperature values
-  summarize(across(c(WaterTemp, Chla), ~ mean(.x, na.rm = TRUE)), .by = c(StationCode, Year, Week)) %>% 
+  # calculate weekly average chlorophyll, water temperature, and specific
+    # conductance values
+  summarize(
+    across(all_of(wq_params), ~ mean(.x, na.rm = TRUE)), 
+    .by = c(StationCode, Year, Week)
+  ) %>% 
   # convert NaN values to NA values
-  mutate(across(c(WaterTemp, Chla), ~ if_else(is.nan(.x), NA_real_, .x))) %>% 
-  # remove records where chlorophyll values are NA
-  drop_na(Chla) %>% 
+  mutate(across(all_of(wq_params), ~ if_else(is.nan(.x), NA_real_, .x))) %>% 
+  # remove rows where all WQ parameters are NA
+  filter(!if_all(all_of(wq_params), is.na)) %>% 
   arrange(StationCode, Year, Week)
 
 
 # Save and Export Data ----------------------------------------------------
 
-# Save daily and weekly average chlorophyll and water temperature data as csv
-  # and rds files
+# Save daily and weekly average chlorophyll, water temperature, and specific
+  # conductance data as csv and rds files
 df_wq_daily_avg %>% write_csv(here("manuscript_synthesis/data/processed/chla_wt_daily_avg_2013-2019.csv"))
 df_wq_daily_avg %>% saveRDS(here("manuscript_synthesis/data/processed/chla_wt_daily_avg_2013-2019.rds"))
 
